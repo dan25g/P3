e633761f8da7199e020a37f2f0d26327ff076b10
dan25g <dangar452000@gmail.com>
2024-10-19 19:05:58 -0400

    post-get-pong

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db = SQLAlchemy(app)
 
 class Directory(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = 'Directories'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=False, nullable=False)
@@ -28,12 +28,52 @@ class Directory(db.Model):
             'name': self.name,
             'emails': self.emails
         }
-    
+
+#Responde simplemente pong
 @app.route('/status', methods=['GET'])
 def status():
     return jsonify({'response': 'pong'})
 
+# Obtener todos los directorios
+@app.route('/directories', methods=['GET'])
+def get_directories():
+    page = request.args.get('page', 1, type=int)
+    per_page = 1
+    directories = Directory.query.paginate(page=page, per_page=per_page, error_out=False)
+    base_url = request.base_url
+    data = {
+        'count': directories.total,
+        'next': f"{base_url}?page={directories.next_num}" if directories.has_next else None,
+        'prev': f"{base_url}?page={directories.prev_num}" if directories.has_prev else None,
+        'results': [
+            {
+                'id': directory.id,
+                'name': directory.name,
+                'emails': directory.emails 
+            } for directory in directories.items
+        ]
+    }
+    return jsonify(data)
 
+
+# Crear directorio
+@app.route('/directories', methods=['POST'])
+def create_directory():
+    try:
+        data = request.get_json()
+        new_directory = Directory(name=data['name'], emails=data['emails'])
+        db.session.add(new_directory)
+        db.session.commit()
+        return make_response(jsonify(new_directory.json()),201)
+    except  Exception as e:
+        return make_response(jsonify({'message': e}), 500)
+    
+
+"""
+falta Obtener un directorio, Actualizar un directorio, 
+Actualizar parcialmente un directorio y Eliminar un directorio
+
+"""
 
 if __name__ == '__main__':
     with app.app_context():
